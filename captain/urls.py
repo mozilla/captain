f13c4eaa291ab6a9e37a836c8c77094739e51ab8
Michael Kelly <mkelly@mozilla.com>
2013-08-13 16:43:49 -0400

    Enable admin interface.

--- captain/urls.py
+++ captain/urls.py
@@ -1,12 +1,18 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import jingo.monkey
 
 
+admin.autodiscover()
+
+
 urlpatterns = patterns('',
     url(r'', include('captain.base.urls')),
+
+    (r'^admin/', include(admin.site.urls)),
 )
 
 
