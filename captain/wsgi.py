047d8dabe9a614cc35de2f32f0ae6cacba3744fd
Michael Kelly <mkelly@mozilla.com>
2013-09-06 10:52:55 -0400

    Setup manage.py and wsgi.py to load the vendor libraries.

--- captain/wsgi.py
+++ captain/wsgi.py
@@ -14,12 +14,20 @@ framework.
 
 """
 import os
+import site
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "captain.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "captain.settings")
+
+# Add the app dir to the python path so we can import manage.
+wsgidir = os.path.dirname(__file__)
+site.addsitedir(os.path.abspath(os.path.join(wsgidir, '../')))
+
+# Manage adds /vendor to the Python path.
+import manage
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
--- manage.py
+++ manage.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 import os
+import site
 import sys
+
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+def path(*a):
+    return os.path.join(ROOT, *a)
+
+
+# Add vendor/lib/python as a sitedir so that libraries included there are in the PATH.
+site.addsitedir(path('vendor/lib/python'))
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "captain.settings")
