c0eea9ecb691e1e08496089337c382d28b5d3beb
Michael Kelly <mkelly@mozilla.com>
2013-12-19 14:10:57 -0500

    By default, log errors to syslog.

--- captain/settings/base.py
+++ captain/settings/base.py
@@ -1,5 +1,6 @@
 # Django settings for captain project common to all environments.
 import os
+import logging
 from os.path import dirname
 
 from django.core.urlresolvers import reverse_lazy
@@ -142,11 +143,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+# Default logging goes to syslog.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -160,11 +157,16 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'syslog': {
+            '()': logging.handlers.SysLogHandler,
+            'facility': logging.handlers.SysLogHandler.LOG_LOCAL7,
+            'filters': ['require_debug_false']
         }
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
+            'handlers': ['syslog'],
             'level': 'ERROR',
             'propagate': True,
         },
